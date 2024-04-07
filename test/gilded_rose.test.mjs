ce89c4c07dab718b71624fb4b16735085da2dd1c
Artjom Savin <artjom.savin@nordhealth.com>
2024-04-07 21:24:13 +0300

    100% coverage

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -2,26 +2,32 @@ import fs from "fs";
 import { describe, expect, test } from "vitest";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
-const fileLocation = "./test/characteristic-test.json";
-
 describe("Gilded Rose", () => {
-  const testData = fs.readFileSync(fileLocation, "utf-8");
+  const data = fs.readFileSync("./test/characteristic-test.json", "utf-8");
+  const dataObj = JSON.parse(data);
 
-  const dataObject = JSON.parse(testData);
-  for (const entry of dataObject) {
-    const expectedInput = entry.input;
-    test(JSON.stringify(expectedInput), () => {
-      const gildedRose = new Shop([new Item(expectedInput.name, expectedInput.sellIn, expectedInput.quality)]);
+  for (const entry of dataObj) {
+    console.log(entry);
+    test(JSON.stringify(entry), () => {
+      const { input, output } = entry;
+      const gildedRose = new Shop([new Item(input.name, input.sellIn, input.quality)]);
       const items = gildedRose.updateQuality();
 
-      const updatedItem = items[0];
+      const item = items[0];
 
-      const expectedOutput = entry.output;
-
-      expect(updatedItem.name).toBe(expectedOutput.name);
-
-      expect(updatedItem.sellIn).toBe(expectedOutput.sellIn);
-      expect(updatedItem.quality).toBe(expectedOutput.quality);
+      expect(item.quality).to.equal(output.quality);
+      expect(item.sellIn).to.equal(output.sellIn);
     });
   }
+
+  test("Empty array", () => {
+    const gildedRose = new Shop();
+    const items = gildedRose.updateQuality();
+    expect(items.length).toBe(0);
+  });
+
+  test("ArrayDeclaration", () => {
+    const gildedRose = new Shop(["Stryker was here"]);
+    expect(() => gildedRose.updateQuality()).toThrowError();
+  });
 });
