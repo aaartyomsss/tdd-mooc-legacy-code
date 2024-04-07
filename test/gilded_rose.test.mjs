f1427df741193c4e6a9f5e9ae0b69c68963e4f4e
Artjom Savin <artjom.savin@nordhealth.com>
2024-04-07 12:24:08 +0300

    Add testing of the system. Mutation 82 percent

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -1,11 +1,27 @@
-import { describe, test } from "vitest";
-import { expect } from "chai";
+import fs from "fs";
+import { describe, expect, test } from "vitest";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
+const fileLocation = "./test/characteristic-test.json";
+
 describe("Gilded Rose", () => {
-  test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
-  });
+  const testData = fs.readFileSync(fileLocation, "utf-8");
+
+  const dataObject = JSON.parse(testData);
+  for (const entry of dataObject) {
+    const expectedInput = entry.input;
+    test(JSON.stringify(expectedInput), () => {
+      const gildedRose = new Shop([new Item(expectedInput.name, expectedInput.sellIn, expectedInput.quality)]);
+      const items = gildedRose.updateQuality();
+
+      const updatedItem = items[0];
+
+      const expectedOutput = entry.output;
+
+      expect(updatedItem.name).toBe(expectedOutput.name);
+
+      expect(updatedItem.sellIn).toBe(expectedOutput.sellIn);
+      expect(updatedItem.quality).toBe(expectedOutput.quality);
+    });
+  }
 });
