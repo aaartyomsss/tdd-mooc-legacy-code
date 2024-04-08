690dec9fb392f2568f05a0f84222f9b559ae4a2b
Artjom Savin <artjom.savin@nordhealth.com>
2024-04-08 19:41:13 +0300

    Add placeholder

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -11,6 +11,8 @@ export class Shop {
     this.items = items;
   }
 
+  updateAgedBrieQuality() {}
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
