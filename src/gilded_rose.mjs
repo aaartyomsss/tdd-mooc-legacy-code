cced26e802fbe0d287d846b75a3cf7f9cda1f34c
Artjom Savin <artjom.savin@nordhealth.com>
2024-04-08 19:49:55 +0300

    Substitute methods instead of previous logic

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -42,20 +42,10 @@ export class Shop {
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
+        if (this.items[i].name === "Aged Brie") {
+          this.updateAgedBrieQuality(i);
+        } else if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
+          this.updateBackstageQuality(i);
         }
       }
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
