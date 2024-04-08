dd8e641c4b24a396a13b52833066cc52f4d349fc
Artjom Savin <artjom.savin@nordhealth.com>
2024-04-08 19:58:43 +0300

    Skip sulfuras right from beginning

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -17,17 +17,17 @@ export class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      this.handleQualityUpdate(i);
-      if (this.items[i].name != SULFURAS) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (this.items[i].name === SULFURAS) {
+        continue;
       }
+      this.handleQualityUpdate(i);
+
+      this.items[i].sellIn = this.items[i].sellIn - 1;
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != BRIE) {
           if (this.items[i].name != BACKSTAGE) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFURAS) {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
+              this.items[i].quality = this.items[i].quality - 1;
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
@@ -76,7 +76,7 @@ export class Shop {
       this.updateAgedBrieQuality(i);
     } else if (this.items[i].name === BACKSTAGE) {
       this.updateBackstageQuality(i);
-    } else if (this.items[i].name !== SULFURAS) {
+    } else {
       this.updateItem(i);
     }
   }
