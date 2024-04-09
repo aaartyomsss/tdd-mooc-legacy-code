2d19bcc8776fa38adefa732e03f5aa147272040c
Artjom Savin <artjom.savin@nordhealth.com>
2024-04-09 19:56:22 +0300

    Additional tests for conjured item

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -98,9 +98,11 @@ export class Shop {
   }
 
   updateConjuredQuality(i) {
+    console.log(this.items[i].quality);
     if (this.items[i].quality - 2 > 0) {
       this.items[i].quality -= 2;
     } else {
+      console.log("Should be here");
       this.items[i].quality = 0;
     }
   }
